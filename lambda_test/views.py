a7209474e59d480e7afbe690b751aac117ead2fe
hww19920718 <331895686@qq.com>
2019-06-02 21:08:42 -0400

    works now

--- lambda_test/views.py
+++ lambda_test/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 import http.client
+import requests
 
 # Create your views here.
 def index(request):
-	urlStr = 'l85qj2flnh.execute-api.us-east-1.amazonaws.com/default/my-function?key1=value1'
-	conn = http.client.HTTPSConnection(urlStr)
-	conn.putrequest('GET', '/')
-	response = conn.getresponse()  
-	print(response.read())
+	ulr_str = 'https://l85qj2flnh.execute-api.us-east-1.amazonaws.com/default/my-function?key1=value1'
+	response = requests.get(ulr_str)
+	# conn = http.client.HTTPSConnection(urlStr)
+	# conn.putrequest('GET', '/')
+	# response = conn.getresponse()  
+	print(response.content)
+	return render(request, 'lambda_test/index.html', {})
+
